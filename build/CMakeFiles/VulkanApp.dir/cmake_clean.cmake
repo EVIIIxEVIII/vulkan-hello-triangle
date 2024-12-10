c64d2d8072b8d55d1aa4edb0dc24f7fcde100dd7
EVIIIxEVIII <xnlyre@protonmail.com>
2024-12-10 21:20:13 +0200

    Start splitting the project into classes

---
+++ build/CMakeFiles/VulkanApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanApp.dir/src/main.cpp.o"
+  "CMakeFiles/VulkanApp.dir/src/main.cpp.o.d"
+  "VulkanApp"
+  "VulkanApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
